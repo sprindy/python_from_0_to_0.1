7d37b0d72336d9e161e291948e521b27b603415d
Sprindy <Sprindy@192.168.0.102>
2020-03-28 10:32:42 +0800

    modify sum to calculate sum of all even number
    Signed-off-by: Sprindy <Sprindy@192.168.0.102>

--- lesson_04/example_code/sum.py
+++ lesson_04/example_code/sum.py
@@ -8,10 +8,12 @@ sum = 0
 i = 1
 #循环执行100次
 while (i <= 100):
+    if (i%2 == 0):
+        sum = sum + i
     i = i + 1
-    sum = sum + i
+
 #打印求和结果
-print('1到101的和sum是', sum)
+print('1到100的偶数的和sum是', sum)
 
 #重新赋值为0
 sum = 0
@@ -22,3 +24,15 @@ for k in range(1, 101):
 
 #打印求和结果
 print('1到100的和sum是', sum)
+
+
+#重新赋值为0
+sum = 0
+print(sum)
+#循环从1加到100
+for k in range(1, 101):
+    if(k%2 == 0):
+        sum = k + sum
+
+#打印求和结果
+print('1到100的偶数的和sum是', sum)
