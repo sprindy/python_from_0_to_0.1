db5b4d907ab752a1127e89ecfd65b76df3753490
Sprindy Liu <sprindy_liu@icloud.com>
2020-04-02 09:04:12 +0800

    add more samples code using function
    Signed-off-by: Sprindy Liu <sprindy_liu@icloud.com>

---
+++ lesson_05/example_code/mux_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+ 
+# 计算面积函数
+def area(width, height) {
+    return width * height
+}
+
+def print_welcome(name):
+    print("Welcome", name)
+ 
+print_welcome("Runoob")
+w = 4
+h = 5
+c1 = area(3, 30)
+print("width =", w, " height =", h, " area =", c1)
