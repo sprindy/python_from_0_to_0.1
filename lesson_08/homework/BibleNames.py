97d5fc2b36313f01d54a3f70a83345e20a0600ea
Sprindy Liu <sprindy_liu@icloud.com>
2020-04-15 21:37:16 +0800

    add homework for lesson 08
    Signed-off-by: Sprindy Liu <sprindy_liu@icloud.com>

---
+++ lesson_08/homework/BibleNames.py
@@ -0,0 +1,20 @@
+
+
+text_file = 'a.txt'
+books_list = [
+'Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy',
+'Joshua', 'Judges', 'Ruth', 'Samuel', 'Kings',
+'Chronicles', 'Ezra', 'Nehemiah', 'Tobit',
+'Judith', 'Esther', 'Maccabees', 'Job',
+'Psalms', 'Proverbs', 'Ecclesiastes', 'Song of Songs',
+'Wisdom', 'Sirach', 'Isaiah', 'Jeremiah',
+'Lamentations', 'Baruch', 'Ezekiel', 'Daniel',
+'Hosea', 'Joel', 'Amos', 'Obadiah',
+'Jonah', 'Micah', 'Nahum', 'Habakkuk',
+'Zephaniah', 'Haggai', 'Zechariah', 'Malachi',
+'Matthew', 'Mark', 'Luke', 'John',
+'Acts', 'Romans', 'orinthians', 'Galatians',
+'Ephesians', 'Philippians', 'Colossians', 'Thessalonians',
+'Timothy', 'Titus', 'Philemon', 'Hebrews',
+'James', 'Peter', 'John', 'Jude',
+'Revelation']
